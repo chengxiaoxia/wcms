6e45166b8c10758f426acd8b7b5bf4a3edd262f5
wangcheng <2605649925@qq.com>
2018-07-10 10:58:48 +0800

    上传wcms项目

---
+++ src/main/java/com/lmfr/wcms/WcmsApplication.java
@@ -0,0 +1,12 @@
+package com.lmfr.wcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcmsApplication.class, args);
+    }
+}
